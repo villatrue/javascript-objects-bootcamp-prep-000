890fa15069e15a5f1ff27d6593cf1b3e8767c2c4
Sean Carter <scarter0313@gmail.com>
2019-01-20 02:45:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {
-  Slowdrive: "Alison"
-  My Bloody Valentine: "Sometimes"
+  "Slowdive": "Alison",
+   "My Bloody Valentine":  "Sometimes"
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
